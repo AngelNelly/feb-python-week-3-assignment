d51e6352e5e77b815831968a8ffe27d495ab87db
AngelNelly <talktoangelnelly@gmail.com>
2025-04-07 17:40:42 +0100

    I finished the assignment and tested my code using Google Colab

---
+++ caldiscount.py
@@ -0,0 +1,16 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent /100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return price
+    
+
+original_price = float(input("Enter the original price of the item: "))
+discount = float(input("Enter the discount percentage: "))
+
+
+final_price = calculate_discount(original_price, discount)
+
+print("The final price after applying discount is:", final_price)
